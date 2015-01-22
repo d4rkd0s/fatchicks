2ff0a07cd5a76064fa0a62d60b0d9a05a6198e1d
Logan Schmidt <logan.schmidt@synercomm.com>
2015-01-22 16:55:26 -0600

    Updated game.js

--- game.js
+++ game.js
@@ -14,14 +14,21 @@ window.onload = function() {
         //  A simple background for our game
         game.add.sprite(0, 0, 'background');
 
-        //create player
-        var player = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
-        player.anchor.setTo(0.5, 0.5);
-
-
+        // The player and its settings
+        player = game.add.sprite(32, game.world.height - 150, 'dude');
+    
         //  We need to enable physics on the player
         game.physics.arcade.enable(player);
-        
+    
+        //  Player physics properties. Give the little guy a slight bounce.
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 300;
+        player.body.collideWorldBounds = true;
+    
+        //  Our two animations, walking left and right.
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+            
     }
     
     function update() {
