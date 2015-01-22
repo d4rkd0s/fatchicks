7025617407b09244689c4b6b1b0e9d940b2f730a
Logan Schmidt <logan.schmidt@synercomm.com>
2015-01-22 16:57:06 -0600

    Updated game.js

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ window.onload = function() {
         game.add.sprite(0, 0, 'background');
 
         // The player and its settings
-        player = game.add.sprite(32, game.world.height - 150, 'dude');
+        player = game.add.sprite(32, game.world.height - 150, 'guy');
     
         //  We need to enable physics on the player
         game.physics.arcade.enable(player);
