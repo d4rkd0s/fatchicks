007ef38026d7361c79aea594e8e79fcf45a465e9
Logan Schmidt <logan.schmidt@synercomm.com>
2015-01-22 16:59:10 -0600

    Physics

--- game.js
+++ game.js
@@ -30,6 +30,37 @@ window.onload = function() {
     
     function update() {
         
-    }
+        //  Reset the players velocity (movement)
+        player.body.velocity.x = 0;
+    
+        if (cursors.left.isDown)
+        {
+            //  Move to the left
+            player.body.velocity.x = -150;
+    
+            
+        }
+        else if (cursors.right.isDown)
+        {
+            //  Move to the right
+            player.body.velocity.x = 150;
+    
+            
+        }
+        else
+        {
+            //  Stand still
+            player.animations.stop();
+    
+            player.frame = 4;
+        }
+        
+        //  Allow the player to jump if they are touching the ground.
+        if (cursors.up.isDown && player.body.touching.down)
+        {
+            player.body.velocity.y = -350;
+        }
+    
+        }
 
 };
