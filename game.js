60208ea5f87c352259b48f1c863171d9e2846bb5
Logan Schmidt <logan.schmidt@synercomm.com>
2015-01-23 17:07:10 -0600

    okay

--- game.js
+++ game.js
@@ -51,15 +51,13 @@ window.onload = function() {
         platforms = game.add.group();
     
         //  We will enable physics for any object that is created in this group
-        
         var ground = platforms.create(0, game.world.height - 92, 'ground');
         
-
         //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
         ground.scale.setTo(1, 1);
     
         //  This stops it from falling away when you jump on it
-        ground.body.immovable = true;
+        //ground.body.immovable = true;
 
 
         //start physics
