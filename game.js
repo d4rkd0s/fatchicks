c572b3a73cc4aa4cb1ac1eccfef3b66acacf81cc
Logan Schmidt <logan.schmidt@synercomm.com>
2015-01-22 18:15:47 -0600

    jump

--- game.js
+++ game.js
@@ -55,7 +55,7 @@ window.onload = function() {
         }
         
         //  Allow the player to jump if they are touching the ground.
-        if (cursors.up.isDown && player.body.touching.down)
+        if (cursors.up.isDown)
         {
             player.body.velocity.y = -350;
         }
