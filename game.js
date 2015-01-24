d2f8a66b9b7bbf426283c1ae9e82ca1096b7e899
Zach <streetbikeford@gmail.com>
2015-01-24 17:37:45 -0600

    Adding some fatty functionality
    Trying to make fatty’s move.

--- game.js
+++ game.js
@@ -6,6 +6,20 @@ window.onload = function() {
     
     var jumping = "0";
 
+    function preload() {
+        
+        game.load.image('guy', 'assets/images/guy.gif');
+        game.load.image('background', 'assets/images/background.jpg');
+        game.load.image('fatty1', 'assets/images/FatGirl1.jpg')
+        //game.load.image('fatty2', 'assets/images/FatGirl2.jpg')
+        //game.load.image('fatty3', 'assets/images/FatGirl3.jpg')
+        //game.load.image('fattyboss', 'assets/images/FatBoss.jpg')
+        game.load.image('ground', 'assets/images/ground.png');
+        cursors = game.input.keyboard.createCursorKeys();
+
+        
+    }
+    
     var health;
     var hotdog;
     var cupcake;
@@ -22,24 +36,6 @@ window.onload = function() {
     var livesText;
     var introText;
 
-    var lookdir;
-
-    function preload() {
-        
-        game.load.image('guy', 'assets/images/guy.png');
-        game.load.image('background', 'assets/images/background.jpg');
-        game.load.image('fatty1', 'assets/images/FatGirl1.jpg')
-        //game.load.image('fatty2', 'assets/images/FatGirl2.jpg')
-        //game.load.image('fatty3', 'assets/images/FatGirl3.jpg')
-        //game.load.image('fattyboss', 'assets/images/FatBoss.jpg')
-        game.load.image('ground', 'assets/images/ground.png');
-        cursors = game.input.keyboard.createCursorKeys();
-        game.load.audio('jumpsound', 'assets/sounds/jump.wav');
-        
-    }
-    
-    
-
 
 
     function create() {
@@ -48,23 +44,20 @@ window.onload = function() {
         game.add.sprite(0, 0, 'background');
 
         //Game Objects
-        player = game.add.sprite(550, game.world.height - 300, 'guy');
+        player = game.add.sprite(450, game.world.height - 500, 'guy');
 
-
-        //sound
-        jumpsound = game.add.audio('jumpsound');
-        jumpsound.allowMultiple = false;
-
-        //hiding this for now its in the way
-        //var fatty1 = game.add.sprite(0, 0, 'fatty1');
+       
+        var image = game.add.sprite(350,game.world.height - 500, 'fatty1');
         
+        game.physics.enable(image, Phaser.Physics.ARCADE);
+        image.body.velocity.x=120;
 
         //Uncaught TypeError: Cannot read property 'velocity' of null
         //fatty1.body.velocity.x=20;
 
         ground = game.add.sprite(0, game.world.height - 92, 'ground');
     
-
+        //Adding some fattys
         //fatty1 = game.add.sprite(450, game.world.height - 300, 'fatty1');
         //fatty2 = game.add.sprite(450, game.world.height - 300, 'fatty2');
         //fatty3 = game.add.sprite(450, game.world.height - 300, 'fatty3');
@@ -79,12 +72,10 @@ window.onload = function() {
         game.physics.arcade.enable(player);
         game.physics.arcade.enableBody(player);
         //Player physics properties. 
-        player.body.bounce.y = 0.3;
-        player.body.gravity.y = 800;
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 600;
         player.body.collideWorldBounds = true;
-        
-        //start the player looking right
-        lookdir = "right";
+    
         
     }
     
@@ -100,17 +91,15 @@ window.onload = function() {
         if (cursors.left.isDown)
         {
             //  Move to the left
-            player.body.velocity.x = -200;
-            //make the player look left
-            lookdir = "left";
+            player.body.velocity.x = -150;
+    
             
         }
         else if (cursors.right.isDown)
         {
             //  Move to the right
-            player.body.velocity.x = 200;
-            //make the player look right
-            lookdir = "right";
+            player.body.velocity.x = 150;
+    
             
         }
         else
@@ -120,21 +109,14 @@ window.onload = function() {
         
 
         //  Allow the player to jump if they are touching the ground.
-        if (cursors.up.isDown && player.y > 430)
+        if (cursors.up.isDown && player.y < 100)
         {
-            jumpsound.play();
-            player.body.velocity.y = -450;
+            
+            player.body.velocity.y = -350;
+
             
         }
     
-        if (game.input.keyboard.isDown(Phaser.Keyboard.SPACE))
-        {
-            ufo.x -= speed;
-            ufo.angle = -15;
-            leftBtn.alpha = 0.6;
-        }
-
-
         }
     function intro (){
     	introText.text = 'Welcome! Feed those fatties!';
